02b961981d8e177de894f03c6997cd9a56ed3865
origer <origer.mir@gmail.com>
2019-09-07 13:30:27 +0900

    first commit

---
+++ hello/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
